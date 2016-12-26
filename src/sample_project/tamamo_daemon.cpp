2d6febb1a1e217e6fb3765c537ffb70d29057f0d
nkawahara <nkawahara@comp.is.uec.ac.jp>
2016-12-26 18:26:17 +0900

    recieve server

---
+++ src/sample_project/tamamo_daemon.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <Poco/Net/Socket.h>
+#include <Poco/Net/TCPServer.h>
+#include <Poco/Net/StreamSocket.h>
+
+#define DEBUG
+#define TIME_BENCH
+
+typedef struct {
+  int number;
+  char name[20];
+} test_opeItem;
+
+
+int main(){
+  Poco::Net::ServerSocket *serv;
+  Poco::Net::StreamSocket *ss;
+
+  serv = new Poco::Net::ServerSocket(33039);
+  serv -> listen();
+  
+  test_opeItem buffer_ope;
+
+  for(;;){
+    ss = new Poco::Net::StreamSocket(serv -> acceptConnection());
+    ss -> setNoDelay(true);
+    std::cout << "Waiting for connection";
+
+    ss -> receiveBytes( &buffer_ope, sizeof(buffer_ope)-1 );    
+    fprintf(stderr, "%d %s",buffer_ope.number, buffer_ope.name);    
+    }
+}
+  
+  
