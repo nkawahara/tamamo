423ff76642fb37a4b862c16f6a868114b61669f7
nkawahara <nkawahara@comp.is.uec.ac.jp>
2017-01-11 11:47:06 +0000

    修正

--- src/install_poco.sh
+++ src/install_poco.sh
@@ -1,19 +1,18 @@
 #/bin/bash
-f [ ! -e ~/usr/lib/libPocoUtil.so ]; then
-    echo "すでにPocoがインストールされています．インストールの必要はありません．"
-    exit
+if [ ! -e ~/usr/lib/libPocoUtil.so ]; then
+    echo "必要なソフトウェアライブラリをインストールします．"
+    wget https://pocoproject.org/releases/poco-1.7.7/poco-1.7.7.tar.gz
+    tar xvf poco-1.7.7.tar.gz
+    cd poco-1.7.7
+    echo "IMPORTANT: Make sure that the path to the build directory does not contain symbolic links. Furthermore, on Mac OS X (or other systems with case insensitive filesystems), make sure that the characters in the path have the correct case. Otherwise you'll get an error saying Current working directory not under $PROJECT_BASE.."
+    ./configure --omit=Data/ODBC,Data/MySQL --prefix=$HOME/usr  --no-tests --no-samples
+    make -j4
+    make install 
+    echo "自分の環境変数に下記を追加してください"
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/usr/lib
+    export PATH=$PATH:$HOME/usr/bin
+    #g++ -I ~/usr/include/ -L ~/usr/lib/  -l PocoFoundationd test.cpp
 fi
-echo "必要なソフトウェアライブラリをインストールします．"
 
-wget https://pocoproject.org/releases/poco-1.7.7/poco-1.7.7.tar.gz
-tar xvf poco-1.7.7.tar.gz
-cd poco-1.7.7
-echo "IMPORTANT: Make sure that the path to the build directory does not contain symbolic links. Furthermore, on Mac OS X (or other systems with case insensitive filesystems), make sure that the characters in the path have the correct case. Otherwise you'll get an error saying Current working directory not under $PROJECT_BASE.."
-./configure --omit=Data/ODBC,Data/MySQL --prefix=$HOME/usr  --no-tests --no-samples
-make -j4
-make install 
-echo "自分の環境変数に下記を追加してください"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/usr/lib
-export PATH=$PATH:$HOME/usr/bin
+echo "Pocoがインストールされています．"
 
-#g++ -I ~/usr/include/ -L ~/usr/lib/  -l PocoFoundationd test.cpp
